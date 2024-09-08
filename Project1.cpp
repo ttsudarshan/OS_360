0a550db45b9b14bd4a1e3dfe6f45c5aa0b873358
Sudarshan <159210386+ttsudarshan@users.noreply.github.com>
2024-09-08 17:58:53 -0500

    sunday

--- Project1.cpp
+++ Project1.cpp
@@ -1,106 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <vector>
-
-#define MAX_INPUT_SIZE 1024
-#define HISTORY_SIZE 5
+#include <cstring>
 
 using namespace std;
 
-void print_prompt();
-void parse_input(char *input, char **command);
-void execute_command(char **command, vector<int>& history);
-void help_command();
-void history_command(const vector<int>& history);
+const string BEAR_ID = "st582s"; 
+
+
+vector<pid_t> history;
+
+
+void phelp();
+void executeCommand(const string& command);
+void printHistory();
+void History(pid_t pid);
 
 int main() {
-    char input[MAX_INPUT_SIZE];
-    char *command[2];  
-    vector<int> history;
+    string input;
 
-    while (1) {
-        print_prompt();
-        
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            printf("\n");
+    while (true) {
+        // Print prompt
+        cout << BEAR_ID << "$ ";
+        getline(cin, input);
+
+        if (input == "exit") {
+            cout << "Exit!" << endl;
             break;
-        }
-
-        // Remove newline character
-        input[strcspn(input, "\n")] = 0;
-
-        parse_input(input, command);
-
-        if (command[0] == NULL) {
-            continue;  // Empty input
-        }
-
-        if (strcmp(command[0], "exit") == 0) {
-            printf("Exit!\n");
-            break;
-        } else if (strcmp(command[0], "help") == 0) {
-            help_command();
-        } else if (strcmp(command[0], "history") == 0) {
-            history_command(history);
+        } else if (input == "help") {
+            phelp();
+        } else if (input == "history") {
+            printHistory();
         } else {
-            execute_command(command, history);
+            executeCommand(input);
         }
     }
 
     return 0;
 }
 
-void print_prompt() {
-    printf("st582s$ ");  
+void phelp() {
+    cout << "//*********************************************************" << endl;
+    cout << "//" << endl;
+    cout << "// <Sudarshan Tiwari!!!!>" << endl;
+    cout << "// CSC360 Operating Systems" << endl;
+    cout << "// Project #1: My Shell - Writing Your Own Shell" << endl;
+    cout << "// This shell supports the following commands: help, exit, history" << endl;
+    cout << "//" << endl;
+    cout << "//*********************************************************" << endl;
 }
 
-// Note: This function should be replaced with the provided parsing code
-void parse_input(char *input, char **command) {
-    command[0] = strtok(input, " \n");
-    command[1] = NULL;
-}
-
-void execute_command(char **command, std::vector<int>& history) {
+void executeCommand(const string& command) {
     pid_t pid = fork();
 
-    if (pid == -1) {
-        perror("fork");
+    if (pid < 0) {
+        cerr << "Fork failed!" << endl;
         return;
-    } else if (pid == 0) {
-        // Child process
-        if (execvp(command[0], command) == -1) {
-            printf("Error: Command could not be executed\n");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // Parent process
+    }
+
+    if (pid == 0) { // Child procss
+        execl("/bin/sh", "sh", "-c", command.c_str(), (char*)nullptr);
+        // If afils
+        cerr << "Error: Command could not be executed" << endl;
+        exit(1);
+    } else { // Praent process
+        History(pid);
         int status;
         waitpid(pid, &status, 0);
-        
-        // Add child PID to history
-        if (history.size() >= HISTORY_SIZE) {
-            history.erase(history.begin());
-        }
-        history.push_back(pid);
     }
 }
 
-void help_command() {
-    printf("//****************************************************\n");
-    printf("//\n");
-    printf("// <Sudarshan Tiwari!!!!>\n");
-    printf("// CSC 360 Operating Systems\n");
-    printf("// Project #1: My Own Shell\n");
-    printf("// This shell supports the following commands: help, exit, history\n");
-    printf("//\n");
-    printf("//****************************************************\n");
+void printHistory() {
+    for (auto pid : history) {
+        cout << pid << endl;
+    }
 }
 
-void history_command(const vector<int>& history) {
-    for (int i = 0; i < history.size(); i++) {
-        printf("%d\n", history[i]);
+void History(pid_t pid) {
+    if (history.size() >= 5) {
+        history.erase(history.begin());
     }
+    history.push_back(pid);
 }
